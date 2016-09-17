0ebec79c7a5c38c2298a9f90960c9057016daecb
Marshall Strong <marshall.strong@protonmail.com>
2016-09-17 16:25:33 -0400

    Add files via upload

---
+++ GameSession.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTac
+{
+    public class GameSession
+    {
+        public void ComputerAI(ref String one, ref String two, ref String three, ref String four, ref String five,
+            ref String six, ref String seven, ref String eight, ref String nine)
+        {
+
+
+        }
+
+        //Draws and updates program screen while game is being played.
+        public void DrawScreen(ref String one, ref String two, ref String three, ref String four, ref String five,
+            ref String six, ref String seven, ref String eight, ref String nine, ref String difficulty, ref Boolean GameRunning, ref int turn)
+        {
+            Console.Clear();
+            //Draw Program Area
+            Console.WriteLine("");
+            Console.WriteLine("   **************************************************");
+            Console.WriteLine("   **                                              **");
+            Console.WriteLine("   **               Welcome to Console             **");
+            Console.WriteLine("   **                * Tic-Tac-Toe *               **");
+            Console.WriteLine("   ** -------------------------------------------- **");
+            Console.WriteLine("   **   Difficulty: {0}                    Turn: {1}   **", difficulty, turn);
+            Console.WriteLine("   **                                              **");
+            Console.WriteLine("   **                    {0} | {1} | {2}                 **", one, two, three);
+            Console.WriteLine("   **                   -----------                **");
+            Console.WriteLine("   **                    {0} | {1} | {2}                 **", four, five, six);
+            Console.WriteLine("   **                   -----------                **");
+            Console.WriteLine("   **                    {0} | {1} | {2}                 **", seven, eight, nine);
+            Console.WriteLine("   **                                              **");
+            Console.WriteLine("   **                                              **");
+            Console.WriteLine("   **                                              **");
+            Console.WriteLine("   **                                              **");
+            Console.WriteLine("   **                                              **");
+            Console.WriteLine("   **                                              **");
+            Console.WriteLine("   **      Please input a letter to select an      **");
+            Console.WriteLine("   **      option:                                 **");
+            Console.WriteLine("   **                                              **");
+            Console.WriteLine("   **                                              **");
+            Console.WriteLine("   **                                              **");
+            Console.WriteLine("   **                                              **");
+            Console.WriteLine("   **************************************************");
+        }
+
+        public void CheckWinner(ref String one, ref String two, ref String three, ref String four, ref String five,
+            ref String six, ref String seven, ref String eight, ref String nine, ref Boolean GameRunning, ref Boolean firstplayer, ref int turn)
+        {
+            string difficulty = "?";//fix this later. Draw Screen needs to reference outside difficulty string variable.
+
+            //Declares the player winner. The game returns to the main menu by setting GameRunning to false and redraws the menu options.
+            if (one == "X" && two == "X" && three == "X" ||
+                (one == "X" && four == "X" && seven == "X") ||
+                (one == "X" && five == "X" && nine == "X") ||
+                (two == "X" && five == "X" && eight == "X") ||
+                (four == "X" && five == "X" && six == "X") ||
+                (seven == "X" && eight == "X" && nine == "X") ||
+                (three == "X" && six == "X" && nine == "X") ||
+                (seven == "X" && five == "X" && three == "X"))
+            {
+                DrawScreen(ref one, ref two, ref three, ref four, ref five, ref six, ref seven, ref eight, ref nine, ref difficulty, ref GameRunning, ref turn);
+                Console.SetCursorPosition(11, 22);
+                Console.WriteLine("You Win. Press any key to return       ");
+                Console.Write("   **      to the main menu.");
+                Console.ReadKey();
+                GameRunning = false;
+                Console.SetCursorPosition(11, 14);
+                Console.WriteLine("New Game:   A.");
+                Console.SetCursorPosition(11, 15);
+                Console.WriteLine("Difficulty: B.");
+                Console.SetCursorPosition(11, 16);
+                Console.WriteLine("Exit:       C.");
+                Console.SetCursorPosition(11, 22);
+                Console.WriteLine("                                     ");
+                Console.WriteLine("   **                                            ");
+
+            }
+            //Declares the Computer the Winner.
+            else if (one == "O" && two == "O" && three == "O" ||
+                (one == "O" && four == "O" && seven == "O") ||
+                (one == "O" && five == "O" && nine == "O") ||
+                (two == "O" && five == "O" && eight == "O") ||
+                (four == "O" && five == "O" && six == "O") ||
+                (seven == "O" && eight == "O" && nine == "O") ||
+                (three == "O" && six == "O" && nine == "O") ||
+                (seven == "O" && five == "O" && three == "O"))
+            {
+                DrawScreen(ref one, ref two, ref three, ref four, ref five, ref six, ref seven, ref eight, ref nine, ref difficulty, ref GameRunning, ref turn);
+                Console.SetCursorPosition(11, 22);
+                Console.WriteLine("The Computer Wins.");
+                Console.ReadKey();
+                GameRunning = false;
+            }
+            //Declares a Draw.
+            else if ((one == "X" || one == "O") &&
+                (two == "X" || two == "O") &&
+                (three == "X" || three == "O") &&
+                (four == "X" || four == "O") &&
+                (five == "X" || five == "O") &&
+                (six == "X" || six == "O") &&
+                (seven == "X" || seven == "O") &&
+                (eight == "X" || eight == "O") &&
+                (nine == "X" || nine == "O"))
+            {
+                DrawScreen(ref one, ref two, ref three, ref four, ref five, ref six, ref seven, ref eight, ref nine, ref difficulty, ref GameRunning, ref turn);
+                Console.SetCursorPosition(11, 22);
+                Console.WriteLine("The Game is a Draw.");
+                Console.ReadKey();
+                GameRunning = false;
+            }
+            else
+            {
+                GameRunning = true;
+            }
+        }
+
+        public void CreateGame(ref Boolean firstplayer, string difficulty, ref Boolean GameRunning)
+        {
+            String one = "1";
+            String two = "2";
+            String three = "3";
+            String four = "4";
+            String five = "5";
+            String six = "6";
+            String seven = "7";
+            String eight = "8";
+            String nine = "9";
+            int turn = 0;
+
+            while (GameRunning == true)
+            {
+                ++turn;
+                //Manage's the player's turn.
+                while (firstplayer == true)
+                {
+                    var ScreenUpdater = new GameSession();
+                    ScreenUpdater.DrawScreen(ref one, ref two, ref three, ref four, ref five, ref six, ref seven, ref eight, ref nine, ref difficulty, ref GameRunning, ref turn);
+                    Boolean MoveMenu = true;
+                    while (MoveMenu == true)
+                    {
+                        Console.SetCursorPosition(11, 19);
+                        Console.WriteLine("Select a number to make your move: ");
+                        Console.WriteLine("   **                             ");
+                        Console.SetCursorPosition(46, 19);
+                        ConsoleKeyInfo input = Console.ReadKey();
+                        if (char.IsDigit(input.KeyChar))
+                        {
+                            String PlayerMoveString = Convert.ToString(input.KeyChar);
+                            if (PlayerMoveString == "1" && one != "X" && one != "O")
+                            {
+                                one = "X";
+                                Console.SetCursorPosition(11, 20);
+                                firstplayer = false;
+                                MoveMenu = false;
+                                var test = new GameSession();
+                                test.CheckWinner(ref one, ref two, ref three, ref four, ref five, ref six, ref seven, ref eight, ref nine, ref GameRunning, ref firstplayer, ref turn);
+                            }
+                            else if (PlayerMoveString == "2" && two != "X" && two != "O")
+                            {
+                                two = "X";
+                                Console.SetCursorPosition(11, 20);
+                                firstplayer = false;
+                                MoveMenu = false;
+                                var test = new GameSession();
+                                test.CheckWinner(ref one, ref two, ref three, ref four, ref five, ref six, ref seven, ref eight, ref nine, ref GameRunning, ref firstplayer, ref turn);
+                            }
+                            else if (PlayerMoveString == "3" && three != "X" && three != "O")
+                            {
+                                three = "X";
+                                Console.SetCursorPosition(11, 20);
+                                firstplayer = false;
+                                MoveMenu = false;
+                                var test = new GameSession();
+                                test.CheckWinner(ref one, ref two, ref three, ref four, ref five, ref six, ref seven, ref eight, ref nine, ref GameRunning, ref firstplayer, ref turn);
+                            }
+                            else if (PlayerMoveString == "4" && four != "X" && four != "O")
+                            {
+                                four = "X";
+                                Console.SetCursorPosition(11, 20);
+                                firstplayer = false;
+                                MoveMenu = false;
+                                var test = new GameSession();
+                                test.CheckWinner(ref one, ref two, ref three, ref four, ref five, ref six, ref seven, ref eight, ref nine, ref GameRunning, ref firstplayer, ref turn);
+                            }
+                            else if (PlayerMoveString == "5" && five != "X" && five != "O")
+                            {
+                                five = "X";
+                                Console.SetCursorPosition(11, 20);
+                                firstplayer = false;
+                                MoveMenu = false;
+                                var test = new GameSession();
+                                test.CheckWinner(ref one, ref two, ref three, ref four, ref five, ref six, ref seven, ref eight, ref nine, ref GameRunning, ref firstplayer, ref turn);
+                            }
+                            else if (PlayerMoveString == "6" && six != "X" && six != "O")
+                            {
+                                six = "X";
+                                Console.SetCursorPosition(11, 20);
+                                firstplayer = false;
+                                MoveMenu = false;
+                                var test = new GameSession();
+                                test.CheckWinner(ref one, ref two, ref three, ref four, ref five, ref six, ref seven, ref eight, ref nine, ref GameRunning, ref firstplayer, ref turn);
+                            }
+                            else if (PlayerMoveString == "7" && seven != "X" && seven != "O")
+                            {
+                                seven = "X";
+                                Console.SetCursorPosition(11, 20);
+                                firstplayer = false;
+                                MoveMenu = false;
+                                var test = new GameSession();
+                                test.CheckWinner(ref one, ref two, ref three, ref four, ref five, ref six, ref seven, ref eight, ref nine, ref GameRunning, ref firstplayer,ref turn);
+                            }
+                            else if (PlayerMoveString == "8" && eight != "X" && eight != "O")
+                            {
+                                eight = "X";
+                                Console.SetCursorPosition(11, 20);
+                                firstplayer = false;
+                                MoveMenu = false;
+                                var test = new GameSession();
+                                test.CheckWinner(ref one, ref two, ref three, ref four, ref five, ref six, ref seven, ref eight, ref nine, ref GameRunning, ref firstplayer, ref turn);
+                            }
+                            else if (PlayerMoveString == "9" && nine != "X" && nine != "O")
+                            {
+                                nine = "X";
+                                Console.SetCursorPosition(11, 20);
+                                firstplayer = false;
+                                MoveMenu = false;
+                                var test = new GameSession();
+                                test.CheckWinner(ref one, ref two, ref three, ref four, ref five, ref six, ref seven, ref eight, ref nine, ref GameRunning, ref firstplayer, ref turn);
+                            }
+                        }
+                    }
+                }
+
+
+
+                //Manage's the Computer's turn.
+                while (firstplayer == false && GameRunning == true)
+                {
+                    var ScreenUpdater2 = new GameSession();
+                    ScreenUpdater2.DrawScreen(ref one, ref two, ref three, ref four, ref five, ref six, ref seven, ref eight, ref nine, ref difficulty, ref GameRunning, ref turn);
+                    Console.SetCursorPosition(11, 19);
+                    Console.WriteLine("Press any key to show the AI move.");
+                    Console.WriteLine("   **                             ");
+                    Console.SetCursorPosition(11, 22);
+                    Console.ReadKey();
+
+                    while (firstplayer == false)
+                    {
+                        Random rnd = new Random();
+                        int RandomMove = rnd.Next(1, 10);
+                        String ComputerMove = Convert.ToString(RandomMove);
+
+                        if (ComputerMove == "1" && one != "X" && one != "O")
+                        {
+                            one = "O";
+                            Console.SetCursorPosition(11, 20);
+                            firstplayer = true;
+                        }
+                        else if (ComputerMove == "2" && two != "X" && two != "O")
+                        {
+                            two = "O";
+                            Console.SetCursorPosition(11, 20);
+                            firstplayer = true;
+                        }
+                        else if (ComputerMove == "3" && three != "X" && three != "O")
+                        {
+                            three = "O";
+                            Console.SetCursorPosition(11, 20);
+                            firstplayer = true;
+                        }
+                        else if (ComputerMove == "4" && four != "X" && four != "O")
+                        {
+                            four = "O";
+                            Console.SetCursorPosition(11, 20);
+                            firstplayer = true;
+                        }
+                        else if (ComputerMove == "5" && five != "X" && five != "O")
+                        {
+                            five = "O";
+                            Console.SetCursorPosition(11, 20);
+                            firstplayer = true;
+                        }
+                        else if (ComputerMove == "6" && six != "X" && six != "O")
+                        {
+                            six = "O";
+                            Console.SetCursorPosition(11, 20);
+                            firstplayer = true;
+                        }
+                        else if (ComputerMove == "7" && seven != "X" && seven != "O")
+                        {
+                            seven = "O";
+                            Console.SetCursorPosition(11, 20);
+                            firstplayer = true;
+                        }
+                        else if (ComputerMove == "8" && eight != "X" && eight != "O")
+                        {
+                            eight = "O";
+                            Console.SetCursorPosition(11, 20);
+                            firstplayer = true;
+                        }
+                        else if (ComputerMove == "9" && nine != "X" && nine != "O")
+                        {
+                            nine = "O";
+                            Console.SetCursorPosition(11, 20);
+                            firstplayer = true;
+                        }
+                        else
+                        {
+                            firstplayer = false;
+                        }
+                        var test = new GameSession();
+                        test.CheckWinner(ref one, ref two, ref three, ref four, ref five, ref six, ref seven, ref eight, ref nine, ref GameRunning, ref firstplayer, ref turn);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,168 @@
+﻿/* Program: Tic Tac Toe
+ * By: Marshall Strong
+ * Modified: 9/17/2016
+ * 
+ * This is a simple console window Tic-Tac Toe game. The AI
+ * is incomplete and currently only makes random moves.  
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTac
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(57, 28);
+
+            String difficulty = "?";
+            String one = "1";
+            String two = "2";
+            String three = "3";
+            String four = "4";
+            String five = "5";
+            String six = "6";
+            String seven = "7";
+            String eight = "8";
+            String nine = "9";
+
+            //Draw Program Area
+            Console.WriteLine("");
+            Console.WriteLine("   **************************************************");
+            Console.WriteLine("   **                                              **");
+            Console.WriteLine("   **               Welcome to Console             **");
+            Console.WriteLine("   **                * Tic-Tac-Toe *               **");
+            Console.WriteLine("   ** -------------------------------------------- **");
+            Console.WriteLine("   **   Difficulty: {0}                              **", difficulty);
+            Console.WriteLine("   **                                              **");
+            Console.WriteLine("   **                    {0} | {1} | {2}                 **", one, two, three);
+            Console.WriteLine("   **                   -----------                **");
+            Console.WriteLine("   **                    {0} | {1} | {2}                 **", four, five, six);
+            Console.WriteLine("   **                   -----------                **");
+            Console.WriteLine("   **                    {0} | {1} | {2}                 **", seven, eight, nine);
+            Console.WriteLine("   **                                              **");
+            Console.WriteLine("   **      New Game:   A.                          **");
+            Console.WriteLine("   **      Difficulty: B.                          **");
+            Console.WriteLine("   **      Exit:       C.                          **");
+            Console.WriteLine("   **                                              **");
+            Console.WriteLine("   **                                              **");
+            Console.WriteLine("   **      Please input a letter to select an      **");
+            Console.WriteLine("   **      option:                                 **");
+            Console.WriteLine("   **                                              **");
+            Console.WriteLine("   **                                              **");
+            Console.WriteLine("   **                                              **");
+            Console.WriteLine("   **                                              **");
+            Console.WriteLine("   **************************************************");
+
+            //Manage user input.
+            Console.SetCursorPosition(19, 20);
+            Boolean MenuOn = true;
+            while (MenuOn == true)
+            {
+                Console.SetCursorPosition(19, 20);
+                ConsoleKeyInfo input = Console.ReadKey();
+                if (char.IsLetter(input.KeyChar))
+                {
+                    String MenuInput = Convert.ToString(input.KeyChar);
+                    //Runs the game.
+                    Boolean GameRunning = true;
+                    if (MenuInput == "A" || MenuInput == "a")
+                    {
+                        Boolean firstplayer; 
+                        Boolean playermovedfirst; //the AI relies on this.
+                        Boolean MenuOn_02 = true;
+                        while (MenuOn_02 == true)
+                        {
+                            //Prompt for first player.
+                            Console.SetCursorPosition(11, 14);
+                            Console.WriteLine("Go first or second?");
+                            Console.SetCursorPosition(11, 15);
+                            Console.WriteLine("Go First:  A. ");
+                            Console.SetCursorPosition(11, 16);
+                            Console.WriteLine("Go Second: B. ");
+
+                            Console.SetCursorPosition(19, 20);
+                            ConsoleKeyInfo input_2 = Console.ReadKey();
+                            if (char.IsLetter(input.KeyChar))
+                            {
+                                String MenuInput_player = Convert.ToString(input_2.KeyChar);
+                                //player moves first.
+                                if (MenuInput_player == "A" || MenuInput_player == "a")
+                                {
+                                    firstplayer = true;
+                                    playermovedfirst = true;
+                                    Console.SetCursorPosition(11, 22);
+                                    var SinglePlayer = new GameSession();
+                                    SinglePlayer.CreateGame(ref firstplayer, difficulty, ref GameRunning);
+                                    MenuOn_02 = false;
+                                }
+                                //computer moves first.
+                                else if (MenuInput_player == "B" || MenuInput_player == "b")
+                                {
+                                    firstplayer = false;
+                                    playermovedfirst = false;
+                                    Console.SetCursorPosition(11, 22);
+                                    var SinglePlayer = new GameSession();
+                                    SinglePlayer.CreateGame(ref firstplayer, difficulty, ref GameRunning);
+                                    MenuOn_02 = false;
+                                }
+                                //Exception handling in case where char is not A, a, B, or b.
+                                else
+                                {
+                                    Console.SetCursorPosition(11, 22);
+                                    Console.Write("Error: try again.");
+                                }
+                            }
+                            /* Exception handling in case where user does not enter a letter
+                             *  in first turn choice.
+                             */
+                            else
+                            {
+                                Console.Write("Error: Try Again.");
+                            }
+                        }
+                    }
+                    /* Allows the user to change the difficulty variable. Difficulty
+                     * works by increasing the chance that the AI will deviate from
+                     * making perfect moves and will instead make a random move.
+                     */
+                    else if (MenuInput == "B" || MenuInput == "b")
+                    {
+                        Console.SetCursorPosition(11, 23);
+                        Console.WriteLine("DEBUG: CONFIRM B.");
+                    }
+                    //Exits the program.
+                    else if (MenuInput == "C" || MenuInput == "c")
+                    {
+                        Console.SetCursorPosition(11, 22);
+                        Console.WriteLine("Thank you for playing.    ");
+                        Console.SetCursorPosition(11, 23);
+                        Console.Write("Press any key to exit.");
+                        MenuOn = false;
+                        Console.SetCursorPosition(33, 23);
+                    }
+                    //Exception handling in cases where char is not a valid choice.
+                    else
+                    {
+                        Console.SetCursorPosition(11, 22);
+                        Console.Write("Error: try again.");
+                    }
+                }
+                //Exception handling in cases where user does not enter a letter.
+                else
+                {
+                    Console.SetCursorPosition(11, 22);
+                    Console.Write("Error: try again.");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
